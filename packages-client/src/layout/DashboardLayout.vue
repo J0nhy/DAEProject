56aa21a8be833ce01d6f81e142dcf4713d8ad2e0
Gonçalo Correia <2211017@my.ipleiria.pt>
2024-01-17 16:34:05 +0000

    Customer list + create front end

--- packages-client/src/layout/DashboardLayout.vue
+++ packages-client/src/layout/DashboardLayout.vue
@@ -26,6 +26,10 @@
         <i class="nc-icon nc-notes"></i>
         <p>Logistics Operator</p>
       </sidebar-link>
+      <sidebar-link to="/admin/customer-list">
+        <i class="nc-icon nc-notes"></i>
+        <p>Customer</p>
+      </sidebar-link>
       <sidebar-link to="/admin/typography">
         <i class="nc-icon nc-paper-2"></i>
         <p>Typography</p>
